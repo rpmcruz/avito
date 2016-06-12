081ab101a73fe2b8be80b436597760b212649bdc
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2016-06-12 11:39:04 +0100

    Bugfix: used brands by mistake.

--- src/features/extract-images-count.py
+++ src/features/extract-images-count.py
@@ -2,9 +2,6 @@
 
 
 def fn(filename, myreader, info, lines):
-    _myreader = myreader.copy()
-
-    from features.text.terms import Brands
-    X1 = Brands(2).transform(_myreader, lines)
-    X2 = Brands(3).transform(_myreader, lines)
-    return ([X1, X2], ['brands-title-dist', 'brands-descr-dist'])
+    from features.image.imagediff import diff_image_count
+    X = diff_image_count(filename, lines)
+    return ([X], ['image-count-diff', 'image-count-both'])
