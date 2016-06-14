2ca861d279cfbbe15432c33585590a7a03194406
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2016-06-14 16:55:36 +0100

    Bug fix: catch exception when loading file fails. Also, aesthetic improvements.

--- src/test/listfeatures.py
+++ src/test/listfeatures.py
@@ -14,7 +14,11 @@ y = np.genfromtxt(filename, int, delimiter=',')
 for file in sorted(os.listdir('../../out')):
     if file.startswith('features-') and file.endswith('-train.csv'):
         filename = os.path.join('../../out', file)
-        X = np.genfromtxt(filename, float, delimiter=',', names=True)
+        try:
+            X = np.genfromtxt(filename, float, delimiter=',', names=True)
+        except ValueError as ex:
+            print 'Error: could not open: %s' % file
+            continue
         for name in X.dtype.names:
             x = X[name]
             if np.sum(np.logical_or(x == 0, x == 1)) == len(x):
@@ -23,16 +27,18 @@ for file in sorted(os.listdir('../../out')):
                 plt.bar((0, 1), (p0, p1), color=('blue', 'red'),
                         tick_label=('y=0', 'y=1'), align='center')
                 plt.ylim(0, 1)
+                plt.title(name + ' = 1')
             else:
                 for dup in (0, 1):
                     color = ('blue', 'red')[dup]
-                    label = str(dup)
+                    label = 'dup = ' + str(dup)
                     if DENSITY:
                         kde = stats.kde.gaussian_kde(x[y == dup])
                         _x = np.linspace(np.amin(x), np.amax(x), 100)
                         plt.plot(_x, kde(_x), color=color, label=label)
+                        plt.legend()
                     else:  # histogram
                         plt.hist(x[y == dup], 10, normed=True, color=color,
                                  label=label, alpha=0.3)
-            plt.title(name)
+                plt.title(name)
             plt.show()
