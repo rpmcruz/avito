b40b5e2bd9e0a94b7bac165b753a1119ab267ba5
Ricardo Cruz <ricardo.pdm.cruz@gmail.com>
2016-06-14 16:56:06 +0100

    Model only using gradient boosting.

--- src/model.py
+++ src/model.py
@@ -4,56 +4,64 @@
 import sys
 sys.dont_write_bytecode = True
 import os
+import csv
 import numpy as np
+from utils.tictoc import tic, toc
 
-N = 50000
+print 'read data...'
+y = np.genfromtxt('../out/y.csv', int)
 
-y = np.genfromtxt('../out/y.csv', int, max_rows=N)
 
-X = []
-files = os.listdir('../out')
-for i, f in enumerate(files):
-    if f.startswith('features-') and f.endswith('-train.csv'):
-        sys.stdout.write('\r%2d%%' % ((100*i)/len(files)))
-        filename = os.path.join('../out', f)
-        _X = np.genfromtxt(filename, delimiter=',', skip_header=1, max_rows=N)
-        if len(_X.shape) == 1:
-            _X = _X[:, np.newaxis]
-        X.append(_X)
-sys.stdout.write('\r            \r')
+def read_data(f):
+    print f
+    filename = os.path.join('../out', f)
+    X = np.genfromtxt(filename, delimiter=',', skip_header=1)
+    if len(X.shape) == 1:
+        X = X[:, np.newaxis]
+    with open(filename, 'rb') as f:
+        header = next(csv.reader(f, quotechar='"'))
+    return (header, X)
+
+tic()
+files = sorted(os.listdir('../out'))
+files = [f for f in files
+         if f.startswith('features-') and f.endswith('-train.csv')]
+names, X = zip(*map(read_data, files))
+toc('read data')
 X = np.concatenate(X, 1)
 
-from sklearn.grid_search import GridSearchCV
+from sklearn.cross_validation import train_test_split
 from sklearn.metrics import roc_auc_score
-from sklearn.ensemble import RandomForestClassifier
 import xgboost as xgb  # to install xgboost: sudo -H pip install --pre xgboost
 
 
 def kaggle_score(m, X, y):
     return roc_auc_score(y, m.predict_proba(X)[:, 1])
 
-
 params = {'max_depth': range(15, 25+1)}
+Xtr, Xts, ytr, yts = train_test_split(X, y)
 
-print 'RandomForest'
-m = GridSearchCV(
-    RandomForestClassifier(100), params, kaggle_score, refit=False, n_jobs=-1)
-for line in sorted(m.fit(X, y).grid_scores_, key=lambda x: -x[1]):
-    print line
-print
+print 'testing...'
 
-print 'XGradientBoosting'
-m = GridSearchCV(
-    xgb.XGBClassifier(n_estimators=100), params, kaggle_score)
-for line in sorted(m.fit(X, y).grid_scores_, key=lambda x: -x[1]):
-    print line
-print
+subsample = 0.05
+subcolsample = np.sqrt(X.shape[1]) / X.shape[1]
+
+for max_depth in xrange(26, 40+1):
+    #m = RandomForestClassifier(100, max_depth=max_depth)
+    tic()
+    m = xgb.XGBClassifier(n_estimators=100, max_depth=max_depth, subsample=0.1,
+                          colsample_bytree=subcolsample)
+    m.fit(Xtr, ytr)
+    pp = m.predict_proba(Xts)[:, 1]
+    toc('%2d %.4f' % (max_depth, roc_auc_score(yts, pp)))
+    sys.stdout.flush()
 
 import matplotlib.pyplot as plt
 plt.ioff()
 xgb.plot_importance(m.best_estimator_)
 plt.savefig('xgb-features.pdf')
 plt.show()
-xgb.plot_tree(m.best_estimator_)
+
+xgb.plot_tree(m)
 plt.savefig('xgb-tree.pdf', dpi=900)
 plt.show()
